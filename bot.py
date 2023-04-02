bd9596f1b234c2653253cc939ca99e046f7eeeb7
Real ManDroid <52342927+realmandroid@users.noreply.github.com>
2023-04-02 05:34:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ def handle_message(update, context):
     response = openai.Completion.create(
         engine=model_engine,
         prompt=user_input,
-        max_tokens=2048,
+        max_tokens=1848,
         temperature=0.5,
         n=1,
         stop=None,
